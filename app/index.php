fececac38cba6d69bdfdc16ef105a8378fe2ab34
EricSefo <nasim2003essousi@gmail.com>
2023-10-23 19:25:46 +0200

    Sync

---
+++ app/index.php
@@ -0,0 +1,2 @@
+<?php
+$paco = "Juliberto";
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo de la Página</title>
+</head>
+<body>
+<h1><?=$paco;?></h1>
+<!-- Aquí puedes agregar el contenido HTML de tu página -->
+</body>
+</html>

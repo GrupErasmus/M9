133250519d288a9aaa6773c7bccff2604a6b6167
EricSefo <nasim2003essousi@gmail.com>
2023-12-04 17:30:16 +0100

    Sync

--- app/index.php
+++ app/index.php
@@ -1,2 +1,10 @@
 <?php
-$paco = "Juliberto";
+
+use framework\App;
+
+$tasks = App::get('database')->selectAll('tasks');
+
+$greeting = "Hello World!";
+
+require '../resources/views/index.blade.php';
+
